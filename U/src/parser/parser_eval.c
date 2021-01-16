e536645617d6b1ed381c57347c8679ee46b792ea
nuFui <tit.poplatnik353535@gmail.com>
2021-01-16 17:17:57 +0100

    expression parser works!

--- U/src/parser/parser_eval.c
+++ U/src/parser/parser_eval.c
@@ -1,7 +1,7 @@
 #include "../../include/parser/parser_eval.h"
 
 // Evaluates binary operation given left, right operand and the node->op->type.
-static struct EvalResult node_binary_eval(node_binary_t *node, struct EvalResult left, struct EvalResult right, int was_minus)
+static struct EvalResult node_binary_eval(node_binary_t *node, struct EvalResult left, struct EvalResult right)
 {
   struct EvalResult res;
   res.result = -1;
@@ -12,7 +12,7 @@ static struct EvalResult node_binary_eval(node_binary_t *node, struct EvalResult
     res.result = left.result + right.result;
     break;
   case TOK_TYPE_SUB:
-    res.result = left.result - (was_minus ? -right.result : right.result);
+    res.result = left.result - right.result;
     break;
   case TOK_TYPE_MUL:
     res.result = left.result * right.result;
@@ -33,20 +33,18 @@ static struct EvalResult node_binary_eval(node_binary_t *node, struct EvalResult
 }
 
 // Evaluates binary tree from leaves up. 
-struct EvalResult node_binary_tree_eval(node_binary_t *mov, int was_minus)
+struct EvalResult node_binary_tree_eval(node_binary_t *mov)
 {
   if (mov->op->type == TOK_TYPE_INT || mov->op->type == TOK_TYPE_FLT)
   {
     struct EvalResult ret;
     ret.code = EVAL_SUCCESS;
     ret.result = atof(mov->op->value);
-    printf("was_minus = %d, result = %f\n", was_minus, ret.result);
     return ret;
   }
-  struct EvalResult l = node_binary_tree_eval(mov->left, was_minus);
-  struct EvalResult r = node_binary_tree_eval(mov->right, was_minus);
-  was_minus = mov->op->type == TOK_TYPE_SUB && *root != mov;
-  struct EvalResult s = node_binary_eval(mov, l, r, was_minus);
+  struct EvalResult l = node_binary_tree_eval(mov->left);
+  struct EvalResult r = node_binary_tree_eval(mov->right);
+  struct EvalResult s = node_binary_eval(mov, l, r);
   if (s.code == EVAL_FAILURE)
   {
     node_binary_tree_delete(*root);
